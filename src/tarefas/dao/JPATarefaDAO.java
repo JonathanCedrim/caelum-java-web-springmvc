c55f9898f22b0c2242563749d77e6df22ddd9eb3
Daniel-Dos <daniel.dias.analistati@gmail.com>
2017-06-17 00:48:18 -0300

    Fix JPA.

--- src/tarefas/dao/JPATarefaDAO.java
+++ src/tarefas/dao/JPATarefaDAO.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import tarefas.model.Tarefa;
 
